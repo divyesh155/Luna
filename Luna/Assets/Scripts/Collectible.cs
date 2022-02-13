910dd4849e56c3e2eb4c60dd7d3d90982407827b
divyesh155 <50515509+divyesh155@users.noreply.github.com>
2022-02-12 21:33:22 -0500

    Made Beta Stage

---
+++ Luna/Assets/Scripts/Collectible.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Collectible : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void OnCollisionEnter2D(Collision2D col)
+    {
+        if (col.gameObject.CompareTag("Player"))
+        {
+        
+            Destroy(gameObject);
+			
+        }
+    }
+
+}
