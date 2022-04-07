e92fdc28ccd405c7e624e5873377530a2b3ad8b8
Angel Cruz <acruzben@my.centennialcollege.ca>
2022-04-07 11:54:14 -0600

    LevelSelect

--- Assets/[Scripts]/MainMenuController.cs
+++ Assets/[Scripts]/MainMenuController.cs
@@ -19,6 +19,22 @@ public class MainMenuController : MonoBehaviour
     {
         SceneManager.LoadScene(0);
     }
+    public void Level1()
+    {
+        SceneManager.LoadScene(1);
+    }
+    public void Level2()
+    {
+        SceneManager.LoadScene(2);
+    }
+    public void Level3()
+    {
+        SceneManager.LoadScene(3);
+    }
+    public void LevelSelect()
+    {
+        SceneManager.LoadScene(6);
+    }
 
     public void ExitTheGame()
     {
